8412fb12cab9e21ccc7f6d7d98a47022b48db23d
Dmitriy_Uran <dmitriy.simonov@infra.ru>
2024-11-07 19:03:13 +0300

    добавил рабочие проекты по C# с ИнфраТК

---
+++ WPF Interface for Cassandra/Cas_1.4/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿
+using HistoryDB;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Cas_1._4.Views;
+// using Cas_1._4.ViewModels;
+
+using static Cas_1._4.Views.ConnectDialog;
+
+// 172.30.221.120
+
+
+namespace Cas_1._4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _cassandraHistory = new CassandraHistory();
+        }
+
+        private CassandraHistory _cassandraHistory;
+       
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            var connectDialog = new ConnectDialog(_cassandraHistory); // Инициализация диалога подключения
+            var dialogResult = connectDialog.ShowDialog(); // Показать диалог и получить результат
+
+            string OurIP = _cassandraHistory._HOST.ToString();
+            OurIP = "IP узла, к которому мы подключены " + OurIP;
+            IPTextBox.Text = OurIP;
+
+
+            if (dialogResult == true)
+            {
+                // Обработка успешного подключения
+
+            }
+            else
+            {
+                // Обработка отмены подключения или ошибки подключения
+            }
+        }
+
+        private void Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика отключения от базы данных
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void InsertData_Click(object sender, RoutedEventArgs e)
+        {
+            var insertDataWindow = new InsertDataWindow(_cassandraHistory);
+            Placeholder.Content = insertDataWindow;
+            // DataContext = new InsertDataWindow(_cassandraHistory);       
+            // InsertDataWindow insertDataWindow = new InsertDataWindow(_cassandraHistory);
+        }
+
+        private void InsertDataAsync_Click(object sender, RoutedEventArgs e)
+        {
+            var insertDataWindow = new InsertDataAsyncWindow(_cassandraHistory);
+            Placeholder.Content = insertDataWindow;
+        }
+
+        private void ViewData_Click(object sender, RoutedEventArgs e)
+        {
+            var histDataQueryWindow = new HistDataQueryWindow(_cassandraHistory);
+            Placeholder.Content = histDataQueryWindow;
+        }
+
+        private void ManageEvents_Click(object sender, RoutedEventArgs e)
+        {
+            // Открытие окна управления событиями
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InitializeHistDBColumns_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeHistDBColumnsWindow initializeHistDBColumnsWindow = new InitializeHistDBColumnsWindow(_cassandraHistory);
+            initializeHistDBColumnsWindow.ShowDialog();
+        }
+
+        private void CreateKeySpace_Click(object sender, RoutedEventArgs e)
+        {
+            var createKeySpaceWindow = new CreateKeySpaceWindow(_cassandraHistory);
+            Placeholder.Content = createKeySpaceWindow;
+            // DataContext = new CreateKeySpaceWindow(_cassandraHistory);
+        }
+
+        private void DeleteNodeTables_Click(object sender, RoutedEventArgs e)
+        {
+            var truncateNodeTablesWindow = new TruncateNodeTablesWindow(_cassandraHistory);
+            Placeholder.Content = truncateNodeTablesWindow;
+        }
+        private void DropNodeTablesClick(object sender, RoutedEventArgs e)
+        {
+            var dropNodeTables = new DropNodeTables(_cassandraHistory);
+            Placeholder.Content = dropNodeTables;
+        }
+    }
+}
---
+++ WPF Interface for Cassandra/Cas_1.4/Views/CreateKeySpaceWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Common;
+using Common.MsgLog;
+using HistoryDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cas_1._4.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateKeySpaceWindow.xaml
+    /// </summary>
+    public partial class CreateKeySpaceWindow : UserControl
+    {
+        private readonly CassandraHistory _cassandraHistory;
+
+        public CreateKeySpaceWindow(CassandraHistory cassandraHistory)
+        {
+            InitializeComponent();
+            _cassandraHistory = cassandraHistory;
+        }
+
+        private void CreateKeyspace_Click(object sender, RoutedEventArgs e)
+        {
+            string keyspace = KeyspaceTextBox.Text;
+            if (int.TryParse(ReplicationFactorTextBox.Text, out int replicationFactor))
+            {
+                MsgLogClass msgLog;
+                bool success = _cassandraHistory.TryCreateKeyspace(keyspace, replicationFactor, out msgLog);
+
+                if (success)
+                {
+                    MessageBox.Show("Keyspace успешно создан.", "Создание Keyspace", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Ошибка при создании Keyspace: {msgLog.LogText}", "Создание Keyspace", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка: Неверный фактор репликации. Пожалуйста, введите целое число.", "Неверный ввод", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}
